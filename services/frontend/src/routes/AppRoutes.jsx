bf6d45bcfeb0f4fe260f5a3af978725339b0cab5
wiktorlemanski <wiktorlem1@wp.pl>
2025-05-16 02:19:03 +0200

    logowanie i rejestracja zakonvzone

--- services/frontend/src/routes/AppRoutes.jsx
+++ services/frontend/src/routes/AppRoutes.jsx
@@ -4,6 +4,7 @@ import Register from "../pages/Register.jsx";
 import Dashboard from "../pages/Admin/Dashboard.jsx";
 import Users from "../pages/Admin/Users.jsx";
 import Orders from "../pages/Admin/Orders.jsx";
+import PrivateRoute from "../components/PrivateRoute.jsx";
 
 export default function AppRoutes(){
     return (
@@ -11,9 +12,9 @@ export default function AppRoutes(){
             <Route path='/' element={<Navigate to='/login' />} />
             <Route path='/login' element={<Login/>}/>
             <Route path='/register' element={<Register/>}/>
-            <Route path='/admin' element={<Dashboard/>}/>
-            <Route path='/admin/users' element={<Users />}/>
-            <Route path='/admin/orders' element={<Orders/>}/>
+            <Route path='/admin' element={<PrivateRoute><Dashboard/></PrivateRoute>}/>
+            <Route path='/admin/users' element={<PrivateRoute><Users /></PrivateRoute>}/>
+            <Route path='/admin/orders' element={<PrivateRoute><Orders/></PrivateRoute>}/>
             <Route path='*' element={<div>404</div>}/>
         </Routes>
     )
